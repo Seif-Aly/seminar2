bf0b502c16a6980fea76d30ca6c610efb73298f6
seif <saly@edu.hse.ru>
2023-09-22 19:13:45 +0300

    Initial Commit

---
+++ seminar2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar2
+//
+//  Created by seif on 2023-09-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
